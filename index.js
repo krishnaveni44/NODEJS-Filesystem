1eb92ffc876f96b6555cc9fe4f25e123cdf3c56a
krishnaveni44 <krishnaveni44@gmail.com>
2022-04-18 16:30:38 +0530

    1

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
- // const port = process.env.PORT || 5000;  
+  const port = process.env.PORT || 5000;  
 const RoomBooked = mongoose.model('users', UserSchema);
 RoomBooked.createIndexes();
   
